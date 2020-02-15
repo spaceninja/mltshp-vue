d544358565677fe5d2aedd4ee8d78344e692d470
Scott Vandehey <fake-scott@spaceninja.com>
2020-02-14 21:03:47 -0800

    store users in state

--- pages/user/_slug.vue
+++ pages/user/_slug.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>User Detail Page: {{ $route.params.slug }}</h1>
     <p>Details about this user.</p>
+    <h1 v-if="isLoading">Loading…</h1>
     <pre>{{ JSON.stringify(user, undefined, 2) }}</pre>
   </div>
 </template>
@@ -10,14 +11,17 @@
 export default {
   computed: {
     user() {
-      return this.$store.state.user.user;
+      return this.$store.getters['user/getUserBySlug'](this.$route.params.slug);
+    },
+    isLoading() {
+      return this.$store.state.loading;
     },
   },
   validate({ params }) {
     return params.slug;
   },
   created() {
-    this.$store.dispatch('user/getUser', this.$route.params.slug);
+    this.$store.dispatch('user/fetchUser', this.$route.params.slug);
   },
 };
 </script>
--- store/user.js
+++ store/user.js
@@ -1,30 +1,48 @@
 import { getFromApi } from '~/services/mltshp';
 
 export const state = () => ({
-  user: {},
+  users: [],
 });
 
 export const mutations = {
-  setUser(state, user) {
-    state.user = user;
+  ADD_USER(state, user) {
+    console.log('[USER STORE] ADD USER', user);
+    state.users.push(user);
   },
 };
 
 export const actions = {
-  async getUser(context, slug) {
+  async fetchUser({ commit }, slug) {
     console.group('[USER STORE] FETCH', slug);
+    const foundUser = this.getters['user/getUserBySlug'](slug);
 
-    const token = this.$auth.getToken(this.$auth.$state.strategy);
-    console.log('[USER STORE] TOKEN', token);
+    if (foundUser) {
+      console.warn('USER ALREADY IN MEMORY!');
+      commit('FINISH_LOADING', null, { root: true }); // in memory already
+    } else {
+      console.log('USER NOT FOUND');
+      commit('START_LOADING', null, { root: true });
 
-    const user = await getFromApi(
-      token,
-      `https://mltshp.com/api/user_name/${slug}`
-    );
-    console.log('[USER STORE] USER', user);
+      const token = this.$auth.getToken(this.$auth.$state.strategy);
+      console.log('[USER STORE] TOKEN', token);
 
-    // Store the user object
-    context.commit('setUser', user);
+      const user = await getFromApi(
+        token,
+        `https://mltshp.com/api/user_name/${slug}`
+      );
+      console.log('[USER STORE] USER', user);
+
+      // Store the user object
+      commit('ADD_USER', user);
+      commit('FINISH_LOADING', null, { root: true });
+    }
     console.groupEnd();
+  },
+};
+
+export const getters = {
+  getUserBySlug: state => slug => {
+    console.log('[USER STORE] GET USER BY SLUG', slug);
+    return state.users.find(user => user.name === slug);
   },
 };
