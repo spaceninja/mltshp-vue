f0fa0774db8566067adbecfdd3494906342d318c
Scott Vandehey <fake-scott@spaceninja.com>
2020-02-17 14:34:38 -0800

    load user object even if cached

--- store/user.js
+++ store/user.js
@@ -19,48 +19,29 @@ export const actions = {
     console.group('[USER STORE] FETCH', slug);
     commit('START_LOADING', null, { root: true });
 
-    // see if the user is already in the store
-    // TODO: small bug here: If an incomplete user was injected (eg, from the
-    // shake, where it only has a single shake link), then it won't ever try to
-    // load more, because it doesn't know anything's missing.
-    // to reproduce, visit a user shake, reload the page, then visit the user.
-    // their other shakes will be missing.
-    // potential solution - add "loaded from source" flag that is only set if
-    // called from the detail page?
-    const foundUser = User.query()
-      .where('name', slug)
-      .first();
+    // load the token
+    const token = this.$auth.getToken(this.$auth.$state.strategy);
+    console.log('TOKEN', token);
 
-    // TODO: remove cache check
-    if (foundUser) {
-      console.log('USER ALREADY IN STATE!');
-      commit('FINISH_LOADING', null, { root: true }); // in memory already
-    } else {
-      console.warn('USER NOT FOUND IN STATE');
+    // request the user from the API
+    const user = await getFromApi(
+      token,
+      `https://mltshp.com/api/user_name/${slug}`
+    );
 
-      // load the token
-      const token = this.$auth.getToken(this.$auth.$state.strategy);
-      console.log('TOKEN', token);
-
-      // request the user from the API
-      const user = await getFromApi(
-        token,
-        `https://mltshp.com/api/user_name/${slug}`
-      );
-
-      // handle errors
-      if (user.error) {
-        console.error('ERROR', user.error.message);
-        console.groupEnd();
-        commit('FINISH_LOADING', null, { root: true });
-        return;
-      }
-
-      // Store the user object
-      console.log('API RESULT', user);
-      commit('ADD_USER', user);
+    // handle errors
+    if (user.error) {
+      console.error('ERROR', user.error.message);
+      console.groupEnd();
       commit('FINISH_LOADING', null, { root: true });
+      return;
     }
+
+    // Store the user object
+    commit('ADD_USER', user);
+
+    console.log('API RESULT', user);
+    commit('FINISH_LOADING', null, { root: true });
     console.groupEnd();
   },
 };
