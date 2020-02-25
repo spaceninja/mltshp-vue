ef5c3f6b44c0edb9b895a2d2145162611926ae1d
Scott Vandehey <fake-scott@spaceninja.com>
2020-02-25 10:03:06 -0800

    add UserDetail component

---
+++ components/UserDetail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>User Object</h2>
+    <pre>{{ JSON.stringify(user, undefined, 2) }}</pre>
+    <template v-if="!user">
+      SKELETON COMPONENT HERE
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
