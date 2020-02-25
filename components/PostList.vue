2e107e968b75b4f5bfaaf8ad38beb29530e7aebb
Scott Vandehey <fake-scott@spaceninja.com>
2020-02-25 09:42:39 -0800

    add PostList component with skeleton

---
+++ components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Posts</h2>
+    <ol v-if="posts">
+      <li v-for="post in posts" :key="post.sharekey">
+        <nuxt-link :to="`/post/${post.sharekey}`">{{
+          post.title || post.name
+        }}</nuxt-link>
+      </li>
+    </ol>
+    <template v-if="posts.length < 1">
+      SKELETON COMPONENT HERE
+    </template>
+    <h3>Posts Array</h3>
+    <pre>{{ JSON.stringify(posts, undefined, 2) }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/shake/_slug.vue
+++ pages/shake/_slug.vue
@@ -2,32 +2,26 @@
   <div>
     <h1>Shake List Page: {{ shake && shake.name }}</h1>
     <p>A list of the most recent posts from this shake.</p>
-    <img v-if="isLoading" src="/images/loading-mltshp.gif" alt="Loading…" />
     <div v-if="error" style="color:red">
       <strong>{{ error.name }}</strong> {{ error.message }}
     </div>
-    <h2>Shake Posts</h2>
-    <ol v-if="posts">
-      <li v-for="post in posts" :key="post.sharekey">
-        <nuxt-link :to="`/post/${post.sharekey}`">{{
-          post.title || post.name
-        }}</nuxt-link>
-      </li>
-    </ol>
     <h2>Shake Object</h2>
     <pre>{{ JSON.stringify(shake, undefined, 2) }}</pre>
-    <h3>Shake Posts Array</h3>
-    <pre>{{ JSON.stringify(posts, undefined, 2) }}</pre>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
 import Shake from '@/models/Shake';
 import Post from '@/models/Post';
+import PostList from '@/components/PostList';
 
 export default {
   validate({ params }) {
     return params.slug;
+  },
+  components: {
+    PostList,
   },
   data() {
     return {
@@ -45,9 +39,6 @@ export default {
       return Post.query()
         .where('shake_ids', array => array.includes(this.shake.id))
         .get();
-    },
-    isLoading() {
-      return this.$store.state.shake.loading || this.$store.state.post.loading;
     },
   },
   created() {
