988da14a6562c367b0eff2cf672cbcbf03dc7412
Scott Vandehey <fake-scott@spaceninja.com>
2023-04-19 23:31:54 -0700

    simplify homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,9 @@
 <template>
   <div>
     <HomePage />
-    <h3>Authentication Overview</h3>
-    <p>See all available authentication & session information below.</p>
-    <pre v-if="status"><span>Status:</span> {{ status }}</pre>
-    <pre v-if="data"><span>Data:</span> {{ data }}</pre>
-    <pre v-if="csrfToken"><span>CSRF Token:</span> {{ csrfToken }}</pre>
-    <pre v-if="providers"><span>Providers:</span> {{ providers }}</pre>
   </div>
 </template>
 
 <script lang="ts" setup>
 definePageMeta({ auth: false });
-const { data, status, getCsrfToken, getProviders } = useAuth();
-
-const providers = await getProviders();
-const csrfToken = await getCsrfToken();
 </script>
